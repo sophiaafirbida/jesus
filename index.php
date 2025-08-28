0062608fc219d3f499e0d951b3b0c32309864112
SOPHIA VITORIA FIRBIDA BRANCO <sophia.5946@aluno.pr.senac.br>
2025-08-28 10:56:15 -0300

    Criando tela de login, criando botao de logout com icone

--- index.php
+++ index.php
@@ -18,6 +18,11 @@
         <nav class="navbar navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="index.php">Crud PHP </a>
+                <form action="login.php" method="GET" class="d-inline">
+                    <button type="submit" class="btn btn-secondary">
+                <i class="fas fa-sign-out-alt"></i> Logout    
+                </button>
+                </form>
             </div>
         </nav>
         <main class="container p-4">
@@ -66,8 +71,8 @@
  
                                         <td><?php echo $row['id']; ?></td>
                                         <td><?php echo $row['title']; ?></td>
-                                        <td><?php echo $row['description']; ?></td>
-                                        <td><?php echo $row['created_at']; ?></td>
+                                        <td><?= substr($row['description'], 0, 20) . '...' ?></td>
+                                       <td><?=date("d/m/Y" , strtotime($row['created_at'])); ?></td>
                                         <td><a href="edit.php?id=<?= $row['id']; ?>" class="btn btn-secondary">
  
                                                 <i class="fas fa-marker"></i>
@@ -81,7 +86,7 @@
                                 }
                             } else {
                                 echo "<tr><td colspan='5'>Nenhuma tarefa encontrada!</td></tr>";
-                            }
+                            }-
                             $conn->close();
                             ?>
  
